8cfd622dc0ced98b7e51c75c45b2b644f14abb10
jeremy-rodrguez <jeremy.rodrguez@yahoo.com>
2020-12-15 16:05:05 -0800

    CRY form_for Lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :artists, only: [:create, :update, :show]
+  # resources :songs, only: [:create, :update, :show]
+  # resources :genres, only: [:create, :update, :show]
+  resources :songs
+  resources :genres
+  resources :artists
 end
